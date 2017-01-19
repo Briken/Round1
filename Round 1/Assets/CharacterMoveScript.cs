03a51db9a4d4f16638430e8b05cc47bee6b91984
Briken <brokenbriken@gmail.com>
2017-01-19 16:15:09 +0000

    init
    basic initialstuff

---
+++ Round 1/Assets/CharacterMoveScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMoveScript : MonoBehaviour {
+
+    public float maxSpeed = 50;
+    public float jumpforce = 10;
+
+    Rigidbody2D charBody;
+
+    
+
+    Vector2 currentVelocity;
+
+    bool faceRight = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+        charBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        currentVelocity = charBody.velocity;
+        charBody.velocity = new Vector2(movement * maxSpeed, currentVelocity.y);
+        Debug.Log("this is your current vel: " + currentVelocity);
+        if ((movement > 0 && !faceRight) || movement <0 && faceRight)
+        {
+            Turn();
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("jumping with this much force: " + jumpforce);
+            charBody.AddForce(transform.up * jumpforce, ForceMode2D.Impulse);
+        }
+    }
+
+    void Turn()
+    {
+        faceRight = !faceRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
---
+++ Round 1/Assets/PlayerControlScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlScript : MonoBehaviour {
+
+    public int playerNum = 1;
+    Transform enemy;
+
+    Rigidbody2D rig2d;
+    Animator anim;
+
+    float horizontal;
+    float vertical;
+
+    public float maxSpeed = 25;
+
+    Vector3 movement;
+
+    bool crouch;
+    bool lPunch;
+    bool hPunch;
+    bool kick;
+    bool jump;
+    bool falling;
+    bool onGround;
+
+
+    public float jumpForce = 20;
+    public float jumpDuration = .1f;
+    float jmpDur;
+    float jmpFrc;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        rig2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();	
+	}
+
+    void Update()
+    {
+        UpdateAnimator();
+    }
+    
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        horizontal = Input.GetAxis("Horizontal" + playerNum.ToString());
+        vertical = Input.GetAxis("Vertical" + playerNum.ToString());
+
+        movement = new Vector3(horizontal, 0, 0);
+
+        crouch = (vertical < -0.1f);
+
+        if (vertical > 0.1f)
+        { }
+
+        if (!crouch)
+        {
+            rig2d.AddForce(movement * maxSpeed);
+        }
+        else
+        {
+            rig2d.velocity = Vector3.zero;
+        }
+	}
+
+    void UpdateAnimator()
+    {
+        anim.SetBool("OnGround", this.onGround);
+        anim.SetBool("HeavyPunch", this.hPunch);
+        anim.SetBool("LightPunch", this.lPunch);
+        anim.SetBool("Kick",this.kick);
+        anim.SetBool("Jump", this.jump);
+        anim.SetBool("Falling", this.falling);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            onGround = true;
+        }
+
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            onGround = false;
+        }
+    }
+}
